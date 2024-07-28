40a899f36a58fa18d8c382d9ccd418976dc32e55
Raphael Wegmann <raphael.wegmann81@gmail.com>
2024-07-28 20:50:35 +0200

    Added GECOS parsing

--- src/system/uname.h
+++ src/system/uname.h
@@ -1,6 +1,9 @@
 #ifndef _SYSTEM_UNAME_H
 #define _SYSTEM_UNAME_H
 
+#define MAX_FIELD_LENGTH 256
+
 int system_get_username(char **username, char **fullname);
+char* get_real_name(const char* gecos);
 
 #endif /* _SYSTEM_UNAME_H */
--- src/system/unix/uname.c
+++ src/system/unix/uname.c
@@ -6,6 +6,39 @@
 #include <utils/str.h>
 #include <zircon.h>
 #include <utils/zerr.h>
+#include <stdlib.h>
+
+
+char* get_real_name(const char* gecos) {
+    char* real_name;
+    const char* comma_pos;
+    size_t length;
+
+    /* Allocate memory for the real name */
+    real_name = (char*)malloc(MAX_FIELD_LENGTH);
+    if (real_name == NULL) {
+        zfatal(-284, "Failed to allocate memory for real name\n: ");
+    }
+
+    /* Find the position of the first comma */
+    comma_pos = strchr(gecos, ',');
+    if (comma_pos != NULL) {
+        /* Copy the substring before the first comma into real_name */
+        length = comma_pos - gecos;
+        if (length >= MAX_FIELD_LENGTH) {
+            length = MAX_FIELD_LENGTH - 1;
+        }
+        strncpy(real_name, gecos, length);
+        real_name[length] = '\0';  /* Null-terminate the string */
+    } else {
+        /* No comma found, copy the whole field */
+        strncpy(real_name, gecos, MAX_FIELD_LENGTH - 1);
+        real_name[MAX_FIELD_LENGTH - 1] = '\0';  /* Null-terminate the string */
+    }
+
+    return real_name;
+}
+
 
 int system_get_username(char **username, char **fullname)
 {
@@ -21,9 +54,16 @@ int system_get_username(char **username, char **fullname)
     return 1;
   }
 
+  char* name = get_real_name(pwd->pw_gecos);
+  if(name == NULL) {
+    zerr("Failed to get real name\n");
+    return 1;
+  }
+
   /* copy username and full name strings */
   *username = z_strdup(pwd->pw_name);
-  *fullname = z_strdup(pwd->pw_gecos);
+  *fullname = z_strdup(name);
+
 
   return 0;
 }
