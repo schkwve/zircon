0ab7ceb80b4e6f8a8a52080c8b5017e8a6640e8c
Raphael Wegmann <raphael.wegmann81@gmail.com>
2024-07-29 23:22:40 +0200

    Added server port and channel selection at startup

--- src/main.c
+++ src/main.c
@@ -16,51 +16,56 @@
 void die(void);
 
 int main(int argc, char** argv) {
-
     UNUSED(argc);
+    UNUSED(argv);
 
-    char *address = malloc(16 * sizeof(char));
-    int port = 0000;
-    char *ip = malloc(64 * sizeof(char));
-    char *channel = malloc(64 * sizeof(char));
-
+    char *address = malloc(MAX_IP_LENGTH * sizeof(char));
     if (address == NULL) {
         zfatal(-286, "Failed to allocate memory");
         return 1;
     }
-    
-    if (argv[1] == NULL) {
-        char *scanf_buffer = malloc(64 * sizeof(char));
-        if (scanf_buffer == NULL) {
-            zfatal(-286, "Failed to allocate memory");
-            return 1;
-        }
-        zinfo("Adress (default: irc.libera.chat): ");
-        scanf("%63s", scanf_buffer);
-        if (scanf_buffer[0] == '\0') {
-            strncpy(ip, "irc.libera.chat", 16);
+
+    int port = 0;
+    char *ip = malloc(64 * sizeof(char));
+    char *channel = malloc(64 * sizeof(char));
+
+    if (ip == NULL || channel == NULL) {
+        zfatal(-286, "Failed to allocate memory");
+        return 1;
+    }
+
+    char input_buffer[64];
+
+    printf("Domain to connect to (default: irc.libera.chat): ");
+    if (fgets(input_buffer, sizeof(input_buffer), stdin) != NULL) {
+        input_buffer[strcspn(input_buffer, "\n")] = '\0'; 
+        if (strlen(input_buffer) > 0) {
+            strncpy(ip, input_buffer, 63);
+            ip[63] = '\0';
         } else {
-            strncpy(ip, scanf_buffer, 16);
+            strncpy(ip, "irc.libera.chat", 64);
         }
-        free(scanf_buffer);
     }
 
-    if (argv[2] == NULL) {
-        zinfo("Port (default: 6667): ");
-        scanf("%d", &port);
-    } else {
-        port = atoi(argv[2]);
+    printf("Port to connect to (default: 6667): ");
+    if (fgets(input_buffer, sizeof(input_buffer), stdin) != NULL) {
+        input_buffer[strcspn(input_buffer, "\n")] = '\0'; 
+        if (strlen(input_buffer) > 0) {
+            port = atoi(input_buffer);
+        } else {
+            port = 6667;
+        }
     }
 
-    if (argv[3] == NULL) {
-        char *buffer = malloc(64 * sizeof(char));
-        zinfo("Channel (default: #aurixos): ");
-        scanf("%63s", buffer);
-        if (buffer[0] == '\0') {
-            strncpy(buffer, "#aurixos", 64);
+    printf("Channel to join (default: #aurixos): ");
+    if (fgets(input_buffer, sizeof(input_buffer), stdin) != NULL) {
+        input_buffer[strcspn(input_buffer, "\n")] = '\0'; 
+        if (strlen(input_buffer) > 0) {
+            strncpy(channel, input_buffer, 63);
+            channel[63] = '\0';
+        } else {
+            strncpy(channel, "#aurixos", 64);
         }
-        strncpy(channel, buffer, 64);
-        free(buffer);
     }
 
     register_handlers();
@@ -70,15 +75,15 @@ int main(int argc, char** argv) {
         return 1;
     }
 
-    resolve_hostname(ip, address, MAX_IP_LENGTH);
+    if (resolve_hostname(ip, address, MAX_IP_LENGTH) != 0) {
+        zerr(0, "Failed to resolve hostname");
+        return 1;
+    }
 
     zinfo("Connecting to %s:%d", address, port);
 
     load_config();
 
-    /* Everything must be loaded from here on */
-
-    /* Assume we want to connect to localhost @ 6667 */
     irc_connect_to(address, port, channel);
 
     while (1) {
@@ -87,10 +92,11 @@ int main(int argc, char** argv) {
             zerr(0, "Failed to allocate memory");
             break;
         }
-        scanf("%1023s", buffer); /* Avoid buffer overflow */
-        if (strstr(buffer, "exit") != NULL) {
-            free(buffer);
-            break;
+        if (fgets(buffer, 1024, stdin) != NULL) { /* Avoid buffer overflow */
+            if (strstr(buffer, "exit") != NULL) {
+                free(buffer);
+                break;
+            }
         }
         free(buffer);
     }
@@ -100,10 +106,17 @@ int main(int argc, char** argv) {
     zinfo("Disconnected from %s:%d", address, port);
     zinfo("Done");
 
+    free(address);
+    free(ip);
+    free(channel);
+
     return 0;
 }
 
+
 void die(void) {
+    irc_send("QUIT :Client is shutting down\r\n");
+    zsucc("Sent QUIT command to server");
     irc_disconnect();
     free_config();
     zsucc("Freed memory and disconnected from server");
